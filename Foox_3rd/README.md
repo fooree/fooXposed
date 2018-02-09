e6608a9b1468ca9924a8df2be2f7f155d7a31d62
Fu <gp490609578@163.com>
2018-02-09 22:49:03 +0800

    show how to hook so file load

---
+++ Foox_3rd/README.md
@@ -0,0 +1,1 @@
+Show how to hook load so files.
---
+++ Foox_3rd/src/main/java/foo/ree/demos/x3rd/MainHook.java
@@ -0,0 +1,39 @@
+package foo.ree.demos.x3rd;
+
+import android.util.Log;
+
+import java.io.File;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+/**
+ *
+ * Hook method {@link java.lang.Runtime}#doLoad(String, ClassLoader)
+ * Created by fooree on 2018/2/6.
+ */
+public class MainHook implements IXposedHookLoadPackage {
+
+    private static final String TAG = MainHook.class.getSimpleName();
+
+    @Override
+    public void handleLoadPackage(LoadPackageParam lp) throws Throwable {
+        if (!"com.leo.myworldstr".equals(lp.packageName)) return;
+
+        XposedHelpers.findAndHookMethod(Runtime.class, "doLoad", String.class, ClassLoader.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                String filename = (String) param.args[0];
+                if (new File(filename).exists()) {
+                    Log.i(TAG, "Loading so file " + filename);
+                } else {
+                    Log.i(TAG, "Loading so file " + filename + ", but the file does not exist");
+                }
+
+//                param.args[0] = "/sdcard/my.so";  // 通过修改第0个参数，可以替换so文件
+            }
+        });
+    }
+}
