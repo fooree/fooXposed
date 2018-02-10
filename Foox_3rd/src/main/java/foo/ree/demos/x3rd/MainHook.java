877dbe7b576ea6e86d2e0a45cfab4ddecfb34770
Fu <gp490609578@163.com>
2018-02-10 11:39:23 +0800

    NONE

--- Foox_3rd/src/main/java/foo/ree/demos/x3rd/MainHook.java
+++ Foox_3rd/src/main/java/foo/ree/demos/x3rd/MainHook.java
@@ -6,11 +6,11 @@ import java.io.File;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
 /**
- *
  * Hook method {@link java.lang.Runtime}#doLoad(String, ClassLoader)
  * Created by fooree on 2018/2/6.
  */
@@ -22,7 +22,7 @@ public class MainHook implements IXposedHookLoadPackage {
     public void handleLoadPackage(LoadPackageParam lp) throws Throwable {
         if (!"com.leo.myworldstr".equals(lp.packageName)) return;
 
-        XposedHelpers.findAndHookMethod(Runtime.class, "doLoad", String.class, ClassLoader.class, new XC_MethodHook() {
+        findAndHookMethod(Runtime.class, "doLoad", String.class, ClassLoader.class, new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 String filename = (String) param.args[0];
@@ -32,7 +32,7 @@ public class MainHook implements IXposedHookLoadPackage {
                     Log.i(TAG, "Loading so file " + filename + ", but the file does not exist");
                 }
 
-//                param.args[0] = "/sdcard/my.so";  // 通过修改第0个参数，可以替换so文件
+//                      param.args[0] = "/sdcard/my.so";  // 通过修改第0个参数，可以替换so文件
             }
         });
     }
