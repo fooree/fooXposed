d9a7e3db4f2337454e87693ec8da2bbb53a478f7
Fu <gp490609578@163.com>
2018-04-06 09:29:56 +0800

    修改说明

--- EasyXp/src/main/java/foo/ree/demos/easyxp/EasyUseDemo.java
+++ EasyXp/src/main/java/foo/ree/demos/easyxp/EasyUseDemo.java
@@ -14,7 +14,7 @@ import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 /**
  * 演示{@link EasyHook}的用法。
  * 相对于来说更佳简单：https://github.com/fooree/fooXposed/blob/master/Foox_05th/src/main/java/foo/ree/demos/x05th/FooxMain.java}
- *
+ *de.robv.android.xposed.XposedHelpers#findAndHookMethod(java.lang.Class, java.lang.String, java.lang.Object...)
  * Created by fooree on 2018/4/6.
  */
 
@@ -26,6 +26,11 @@ public class EasyUseDemo extends EasyHook {
 
     @Override
     public void onLoadPackage(XC_LoadPackage.LoadPackageParam lpp) {
+        // 注册Hook代码，避免ClassNotFoundError，
+        // 写法和调用findAndHookMethod保持一致，
+        // 不必再关心DexClassLoader的问题
+        // 可以通过这种方式注册很多很多hook代码
+        // 主dex中的类也可以通过这种方式注册Hook
         hook("foo.ree.demos.x4th01.Base64Util", new OnLoad() {
             @Override
             public void executeHook(Class<?> cls) {
